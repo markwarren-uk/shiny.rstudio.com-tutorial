7cd503b3843e2e7814eee5603c8952940e83eb7d
markwarren-uk <amarkwarren@gmail.com>
2021-02-17 07:24:52 +0000

    Shiny example script added and notes to 04-isolate

--- part-2-code/04-isolate.R
+++ part-2-code/04-isolate.R
@@ -6,7 +6,7 @@ ui <- fluidPage(
   sliderInput(inputId = "num", 
     label = "Choose a number", 
     value = 25, min = 1, max = 100),
-  textInput(inputId = "title", 
+  textInput(inputId = "title", #USER CAN OVERWRITE TITLE
     label = "Write a title",
     value = "Histogram of Random Normal Values"),
   plotOutput("hist")
@@ -14,8 +14,8 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   output$hist <- renderPlot({
-    hist(rnorm(input$num), 
-         main = isolate(input$title))
+    hist(rnorm(input$num), #NO CALL TO DATA() THIS TIME
+         main = isolate(input$title))  #MAINLY TO SHOW HOW TO ISOLATE REACTIVITY FROM TEXT INPUT
   })
 }
 
